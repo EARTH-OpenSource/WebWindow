5b967de304ec7bc6635ffc03ef429436700f59fc
TarochanChannel <gakutyn.2020@gmail.com>
2022-01-23 12:51:52 +0900

    ほぼ完成

---
+++ index.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const { program } = require("commander");
+program.version("1.0.0", "-v, --version", "バージョンを表示します");
+program
+    .option("-u --url <letter>", "URLをしていします")
+    .option("-d --debug", "デバッグを有効にします")
+    .option("-w --width <number>", "ウインドウの横幅を指定します")
+    .option("-h --height <number>", "ウインドウの縦幅を指定します")
+    .option("-f --fullscreen", "ウインドウをフルスクリーンにします")
+    .option("-web --webpreferences <letter>", "webPreferencesを設定します");
+
+const createWindow = (url = "https://earth.renorari.net/", width = 800, height = 600, webPreferences = {}, fullscreen = false, debug = null) => {
+    const mainWindow = new BrowserWindow({
+        width: width,
+        height: height,
+        webPreferences: webPreferences,
+        autoHideMenuBar: true,
+        fullscreen: fullscreen
+    });
+    mainWindow.removeMenu();
+    mainWindow.loadURL(url);
+    if (debug) mainWindow.webContents.openDevTools();
+    return mainWindow;
+};
+
+app.whenReady().then(() => {
+    program.parse(process.argv);
+    const options = program.opts();
+
+    createWindow(options.url, Number(options.width), Number(options.height), JSON.stringify(options.webPreferences), options.fullscreen, options.debug);
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
