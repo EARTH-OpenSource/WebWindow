108ca6e11f528fb8851a3abfd65285e6750c06b2
TarochanChannel <gakutyn.2020@gmail.com>
2022-01-23 15:56:52 +0900

    title引数追加

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const { app, BrowserWindow, Menu } = require("electron");
 const { program } = require("commander");
-program.version("1.0.0", "-v, --version", "バージョンを表示します");
+program.version("1.0.1", "-v, --version", "バージョンを表示します");
 program
     .option("-u --url <letter>", "URLをしていします")
     .option("-d --debug", "デバッグを有効にします")
     .option("-w --width <number>", "ウインドウの横幅を指定します")
     .option("-h --height <number>", "ウインドウの縦幅を指定します")
+    .option("-t --title <letter>", "ウインドウのタイトルを指定します")
     .option("-f --fullscreen", "ウインドウをフルスクリーンにします")
     .option("-web --webpreferences <letter>", "webPreferencesを設定します");
 
-const createWindow = (url = "https://earth.renorari.net/", width = 800, height = 600, webPreferences = {}, fullscreen = false, debug = null) => {
+const createWindow = (url = "https://earth.renorari.net/", width = 800, height = 600 , title = "WebWindow", webPreferences = {}, fullscreen = false, debug = null) => {
     const mainWindow = new BrowserWindow({
         width: width,
         height: height,
         webPreferences: webPreferences,
         autoHideMenuBar: true,
         fullscreen: fullscreen,
-        title: "WebWindow"
+        title: title
     });
     mainWindow.removeMenu();
     mainWindow.loadURL(url);
@@ -28,7 +29,7 @@ app.whenReady().then(() => {
     program.parse(process.argv);
     const options = program.opts();
 
-    createWindow(options.url, Number(options.width), Number(options.height), JSON.stringify(options.webPreferences), options.fullscreen, options.debug);
+    createWindow(options.url, Number(options.width), Number(options.height),options.title, JSON.stringify(options.webPreferences), options.fullscreen, options.debug);
     app.on("activate", () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
